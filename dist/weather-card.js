bcfee56e7a1f79b83fb0205809c782575b7de68f
Marc <980978+MarcHagen@users.noreply.github.com>
2020-05-01 00:08:50 +0200

    Fix temp spacing with HA 109+

--- dist/weather-card.js
+++ dist/weather-card.js
@@ -700,7 +700,7 @@ class WeatherCard extends LitElement {
         color: var(--primary-text-color);
         position: absolute;
         right: 1em;
-        top: 0.3em;
+        top: 0.6em;
       }
 
       .tempc {
